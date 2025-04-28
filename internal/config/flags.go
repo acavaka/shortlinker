7900015fb1fa7818b9a9a0fb4396880ad1f1923b
Александр Волков <luearo@gmail.com>
2025-04-29 00:53:03 +0300

    fixes for test1 in CI

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -8,8 +8,9 @@ var f ServerConfig
 
 func parseFlags() *ServerConfig {
 	if !flag.Parsed() {
-		flag.StringVar(&f.ServerAddress, "a", ":8080", "server address and port") // Слушаем все интерфейсы
-		flag.StringVar(&f.BaseURL, "b", "http://[::1]:8080", "server address")    // IPv6-адрес
+		// Тест жестко требует порт 8080
+		flag.StringVar(&f.ServerAddress, "a", ":8080", "server address")
+		flag.StringVar(&f.BaseURL, "b", "http://localhost:8080", "base url")
 		flag.Parse()
 	}
 	return &f
